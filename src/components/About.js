4c45aa8fe43ff97f880214366f0ab9dae38cdbcf
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-09-25 19:22:58 -0700

    change to the Intro

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import Section from './UI/Section'
-import H1 from './UI/H1'
-import { aboutDescription } from '../utility/descriptions.js'
-import { firstOffsetDesign } from '../utility/sectionDesigns.js'
+import Section from './UI/Section';
+import H1 from './UI/H1';
+import { aboutDescription } from '../utility/descriptions.js';
+import { firstOffsetDesign } from '../utility/sectionDesigns.js';
 
-const secId = 'about'
+const secId = 'about';
 
 const about = () => (
-	<Section design={firstOffsetDesign} sectionId={secId}>
-		<div className="col-md">
-			<H1>Nelson Hsu</H1>
-			<p>{aboutDescription}</p>
-		</div>
-	</Section>
+  <Section design={firstOffsetDesign} sectionId={secId}>
+    <div className="col-md-12">
+      <H1>Nelson Hsu</H1>
+      <p>{aboutDescription}</p>
+    </div>
+  </Section>
+);
 
-)
-
-export default about
+export default about;
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import './Intro.css'
+import './Intro.css';
 
 const intro = () => (
-	<header id="intro-section">
+  <header id="intro-section">
     <div className="dark-overlay">
-        <div className="row h-100">
-          <div className="col my-auto text-center text-white">
-            <h1 className="display-1">Hello There!</h1>
-            <h2>Welcome To My Website</h2>
-          </div>
+      <div className="row h-100">
+        <div className="col my-auto text-center text-white">
+          <h1 className="display-1">React Developer</h1>
+          <h1>Welcome To My Website</h1>
         </div>
       </div>
-	</header>
-)
+    </div>
+  </header>
+);
 
-export default intro
+export default intro;
